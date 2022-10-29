40d3a4e463a584be13adb9cef8d0a3a73529d78d
Kevin Wade <kevinrwade@gmail.com>
2022-10-29 16:14:03 -0500

    Remove automatic db seed

--- apps/backend/src/modules/db.ts
+++ apps/backend/src/modules/db.ts
@@ -5,21 +5,3 @@ const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
 export default db;
 
 export const genId = () => nanoid(16);
-
-const seedDatabase = async () => {
-  if ((await db.submission.count()) === 0) {
-    await db.submission.createMany({
-      data: [
-        {
-          id: genId(),
-          submittedAt: new Date(),
-          data: {
-            name: 'Kevin Wade',
-            twitter: 'kevinwwwade',
-          },
-        },
-      ],
-    });
-  }
-};
-seedDatabase();
